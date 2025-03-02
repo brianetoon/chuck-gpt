24a7ed73763d9d21ffb91221a33da60c77726e3d
Brian Toon <brian.elliot.toon@gmail.com>
2025-03-02 14:16:29 -0700

    scss

---
+++ src/components/SiteHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function SiteHeader() {
+  return (
+    <header>
+      <span>Chuck GPT</span>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Chat</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
