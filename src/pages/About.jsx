4f297680a2062222dc3810b1fdb24a3741f49467
Brian Toon <brian.elliot.toon@gmail.com>
2025-03-04 08:17:52 -0700

    about & favorites updates

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,8 @@
 export default function About() {
   return (
-    <div>
+    <div className="flow margin-block-s">
       <h2 className="heading-2">About</h2>
+      <p>Coming soon...</p>
     </div>
   )
 }
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Favorites() {
   return (
-    <div>
+    <div className="flow margin-block-s">
       <h2 className="heading-2">Favorites</h2>
+      <p>Coming soon...</p>
     </div>
   )
 }
